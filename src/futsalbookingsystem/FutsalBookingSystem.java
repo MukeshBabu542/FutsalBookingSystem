fbbdeee27f68cec0798f5625a3e0a903a145190f
NO-ONE <mergichadmergi@gmail.com>
2025-05-21 18:29:24 +0545

    Registration

--- src/futsalbookingsystem/FutsalBookingSystem.java
+++ src/futsalbookingsystem/FutsalBookingSystem.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package futsalbookingsystem;
 
 import view.RegistrationView;
@@ -10,7 +7,6 @@ import view.RegistrationView;
 public class FutsalBookingSystem {
 
     public static void main(String[] args) {
-        // TODO code application logic here
         RegistrationView RegistrationViewFrame = new RegistrationView(); 
         RegistrationViewFrame.setVisible(true);
         RegistrationViewFrame.pack();

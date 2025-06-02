09b96ffc9d775bbe9416a0bce9f47d9e3a7a69f8
Mukesh-coder7 <mukeshbabuacharyaS@gmail.com>
2025-06-02 20:55:10 +0545

    Login controller added

--- src/futsalbookingsystem/controller/LoginController.java
+++ src/futsalbookingsystem/controller/LoginController.java
@@ -4,10 +4,57 @@
  */
 package futsalbookingsystem.controller;
 
+import futsalbookingsystem.dao.UserDao;
+import futsalbookingsystem.model.LoginRequest;
+import futsalbookingsystem.model.UserData;
+import futsalbookingsystem.view.LoginView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author lenovo
  */
 public class LoginController {
-    
+    private LoginView view;
+    public LoginController(LoginView view){
+        this.view = view;
+        LoginUser loginUser = new LoginUser();
+        this.view.loginUser(loginUser);
+        ResetPassword resetPass = new ResetPassword();
+        this.view.forgetPassword(resetPass);
+        
+        
+    }
+    public void open(){
+        view.setVisible(true);
+    }
+    public void close(){
+        view.dispose();
+    }
+    class LoginUser implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String email = view.getEmailTextField().getText();
+            String password = String.valueOf(view.getPasswordField().getPassword());
+            if (email.isEmpty()||password.isEmpty()){
+                JOptionPane.showMessageDialog(view,"Fill in all the fields");
+            } else {
+               LoginRequest loginReq = new LoginRequest(email,password);
+               UserDao userDao = new UserDao();
+               UserData user = userDao.login(loginReq);
+               if(user==null){
+                   JOptionPane.showMessageDialog(view,"Login failed");
+                   
+//               }else{
+//                   DashboardView dashboardView = new DashboardView();
+//                   DashboardController dashboardController = new DashboardController(dashboardView,user);
+//                   dashboardController.open();
+//                   close();
+               }
+            }
+        }
+    }
 }

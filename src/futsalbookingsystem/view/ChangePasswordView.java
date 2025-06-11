ebc3ff218991308b0781b20a8d7a620ae4556a9f
NO-ONE <mergichadmergi@gmail.com>
2025-06-11 13:11:17 +0545

    dashboard and different tabs

--- src/futsalbookingsystem/view/ChangePasswordView.java
+++ src/futsalbookingsystem/view/ChangePasswordView.java
@@ -10,12 +10,12 @@ import java.awt.Color;
  *
  * @author HP
  */
-public class ForgotPasswordView extends javax.swing.JFrame {
+public class ChangePasswordView extends javax.swing.JFrame {
 
     /**
      * Creates new form ForgotPasswordView
      */
-    public ForgotPasswordView() {
+    public ChangePasswordView() {
         initComponents();
     }
 
@@ -185,20 +185,21 @@ public class ForgotPasswordView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ForgotPasswordView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChangePasswordView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ForgotPasswordView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChangePasswordView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ForgotPasswordView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChangePasswordView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ForgotPasswordView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChangePasswordView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ForgotPasswordView().setVisible(true);
+                new ChangePasswordView().setVisible(true);
             }
         });
     }
